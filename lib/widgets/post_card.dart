925036ccbe17eb8782dfa7aefc52ea61dfe032a4
rahmedd <rahmedd@umich.edu>
2023-07-01 16:30:15 -0400

    Add responsive UI

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -49,8 +49,14 @@ class _PostCardState extends State<PostCard> {
   @override
   Widget build(BuildContext context) {
     final model.User user = Provider.of<UserProvider>(context).getUser;
+    final width = MediaQuery.of(context).size.width;
     return Container(
-      color: mobileBackgroundColor,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: width > webScreenSize ? secondaryColor : mobileBackgroundColor,
+        ),
+        color: mobileBackgroundColor,
+      ),
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: Column(
         children: [
